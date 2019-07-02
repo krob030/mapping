5c75a0d2e79a8c3470e81fd520abc8ada461da06
krob030 <krob030@gmail.com>
2019-07-02 02:15:07 -0500

    first

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3JvYjAwMzAiLCJhIjoiY2p4MWc2dDlnMDlpMzN6cGQzYjl2ejdiMSJ9.cTDNPR8GjgyVRkp3D3gjcQ";
---
+++ logic.js
@@ -0,0 +1,63 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+  
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + feature.properties.mag + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+// Define variables for our tile layers
+var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+ // Set up the legend
+ var legend = L.control({ position: "bottomright" });
+ legend.onAdd = function() {
+   var div = L.DomUtil.create("div", "info legend");
+   var labels = [];
+
+   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+   return div;
+ };
+
+ // Adding legend to the map
+ legend.addTo(myMap);
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 6
+    //layers: [streetmap, earthquakes]
+  });
+
+}
